17cbf5b4cb6c5b93e7d00d66e5707130bc682a1e
Hidetake Iwata <int128@gmail.com>
2019-02-17 14:41:46 +0900

    Add query for pull requests

--- main.go
+++ main.go
@@ -10,16 +10,32 @@ import (
 )
 
 func run(ctx context.Context, client *githubv4.Client) error {
-	var q struct {
-		Viewer struct {
-			Login     githubv4.String
-			CreatedAt githubv4.DateTime
+	{
+		var q struct {
+			Repository struct {
+				PullRequests struct {
+					Nodes []struct {
+						Number int
+					}
+				} `graphql:"pullRequests(states: $states, first: 1, headRefName: $head, baseRefName: $base)"`
+			} `graphql:"repository(owner: $owner, name: $repo)"`
 		}
+		v := map[string]interface{}{
+			"owner": githubv4.String("octocat"),
+			"repo":  githubv4.String("Spoon-Knife"),
+			"states": []githubv4.PullRequestState{
+				githubv4.PullRequestStateOpen,
+				githubv4.PullRequestStateClosed,
+				githubv4.PullRequestStateMerged,
+			},
+			"head": githubv4.String("example"),
+			"base": githubv4.String("master"),
+		}
+		if err := client.Query(ctx, &q, v); err != nil {
+			return errors.Wrapf(err, "error while query")
+		}
+		log.Printf("%+v", q)
 	}
-	if err := client.Query(ctx, &q, nil); err != nil {
-		return errors.Wrapf(err, "error while query")
-	}
-	log.Printf("%+v", q)
 	return nil
 }
 
